fd0a09ce96e8955b7cf235d006cf11082bf9878a
matthewjheaney <matthewjheaney@google.com>
2010-09-16 16:46:09 -0400

    changed names of resource files
    Change-Id: I8fbdae62114659cef11e490cc5c39684dc71d4fd

--- mediafoundation/webmmfvp8dec/resource.h
+++ mediafoundation/webmmfvp8dec/resource.h
@@ -1,6 +1,7 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by webmmfvp8dec_2008.rc
+// Used by webmmfvp8dec.rc
+//
 
 // Next default values for new objects
 // 
