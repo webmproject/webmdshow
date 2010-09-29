8e0e633a3b026d4e7d661af82942ea6f7ad0bfe5
tomfinegan <tomfinegan@google.com>
2010-09-29 12:59:16 -0400

    Fix strings passed to die and add "xiph" to ogg/vorbis functions.
    1. It does not help to put $1 in a msg when trying to let the user know that
    a positional parameter was empty.
    2. The "xiph" bit is for the sake of differentiating the xipg related
    functions from the libvpx functions that will be added in the next commit.
    3. Wrapped xiph commands in block controlled by disable_xiph_builds flag.
    
    Change-Id: Ifed4bc890e42ed4600e2260e1a43c08df3a8a323

--- third_party/update_libs.sh
+++ third_party/update_libs.sh
@@ -19,6 +19,7 @@ patches_dir="patches"
 devenv_outdir_array=(win32/debug x64/debug win32/release x64/release)
 install_dir_array=(x86/debug x64/debug x86/release x64/release)
 
+disable_xiph_builds="" # disables ogg and vorbis builds
 function die() {
   if [[ -n "$1" ]]; then
     echo "ERROR> ($1) exiting!"
@@ -31,7 +32,7 @@ function die() {
 function wget_and_untar() {
   local url="$1"
   if [[ -z "$url" ]]; then
-    die "wget_and_untar: no url specified [$1 empty]"
+    die "wget_and_untar: no url specified [arg1 empty]"
   fi
   local oldIFS="${IFS}"
   IFS=/
@@ -40,50 +41,46 @@ function wget_and_untar() {
   #echo "outfile_url_array=${outfile_url_array[@]}"
   IFS="${oldIFS}"
   local last_array_element=${#outfile_url_array[@]}-1
-  local outfile="${outfile_url_array[${last_array_element}]}"
+  local outfile="${outfile_url_array[arg{last_array_element}]}"
   wget "${url}"
   if [[ ! -e "${outfile}" ]]; then
-    die "wget_and_untar: download failed [$url]"
+    die "wget_and_untar: download failed [argurl]"
   fi
   local outfiles=$(tar xvzf "${outfile}")
-  
-  # echo output dir 
+
+  # echo output dir
   #  - there's probably a better way to do this, but here's to fast and ugly!
   echo "${outfiles}" | tr \[:space:\] \\n | head -n1
 }
 
-function build_lib() {
+function build_xiph_lib() {
   local libdir="$1"
   if [[ -z "$libdir" ]]; then
-    die "build_lib: no libdir specified [$1 empty]"
+    die "build_xiph_lib: no libdir specified [arg1 empty]"
   fi
   local solution="$2"
   if [[ -z "$solution" ]]; then
-    die "build_lib: no solution specified [$2 empty]"
+    die "build_xiph_lib: no solution specified [arg2 empty]"
   fi
   local project="$3"
   if [[ -z "$project" ]]; then
-    die "build_lib: no project specified [$3 empty]"
+    die "build_xiph_lib: no project specified [arg3 empty]"
   fi
   # need vs2008 in your path for this bit
   for config in ${build_config_array[@]}; do
     devenv.com ${libdir}win32/VS2008/${solution}.sln -Project ${project} \
 -Rebuild "${config}"
   done
-  #devenv.com ${libdir}win32/VS2008/${solution}.sln -Project ${project} -Rebuild "Debug|win32"
-  #devenv.com ${libdir}win32/VS2008/${solution}.sln -Project ${project} -Rebuild "Debug|x64"
-  #devenv.com ${libdir}win32/VS2008/${solution}.sln -Project ${project} -Rebuild "Release|win32"
-  #devenv.com ${libdir}win32/VS2008/${solution}.sln -Project ${project} -Rebuild "Release|x64"
 }
 
-function fix_project() {
+function fix_xiph_project() {
   local projdir="$1"
   if [[ -z "$projdir" ]]; then
-    die "fix_project: no projdir specified [$1 empty]"
+    die "fix_xiph_project: no projdir specified [arg1 empty]"
   fi
   local project="$2"
   if [[ -z "$project" ]]; then
-    die "fix_project: no project specified [$2 empty]"
+    die "fix_xiph_project: no project specified [arg2 empty]"
   fi
   sed -i -e "s/RuntimeLibrary=\"3\"/RuntimeLibrary=\"1\"/g" \
 -e "s/RuntimeLibrary=\"2\"/RuntimeLibrary=\"0\"/g" \
@@ -95,22 +92,22 @@ ${projdir}/${project}.vcproj
   fi
 }
 
-function install_files() {
+function install_xiph_files() {
   local project="$1"
   if [[ -z "$project" ]]; then
-    die "install_files: no project specified [$1 empty]"
-  fi 
+    die "install_xiph_files: no project specified [arg1 empty]"
+  fi
   local src_dir="$2"
   if [[ -z "$src_dir" ]]; then
-    die "install_files: no src_dir specified [$2 empty]"
-  fi 
+    die "install_xiph_files: no src_dir specified [arg2 empty]"
+  fi
   local target_dir="$3"
   if [[ -z "$target_dir" ]]; then
-    die "install_files: no target_dir specified [$3 empty]"
-  fi 
+    die "install_xiph_files: no target_dir specified [arg3 empty]"
+  fi
   local inc_dir="$4"
   if [[ -z "$inc_dir" ]]; then
-    die "install_files: no inc_dir specified [$4 empty]"
+    die "install_xiph_files: no inc_dir specified [arg4 empty]"
   fi
 
   # remove old includes
@@ -125,27 +122,33 @@ function install_files() {
   # and the new libs
   for (( lib_index=0; lib_index < ${#install_dir_array[@]}; lib_index++ )); do
     cp ${src_dir}/win32/VS2008/${devenv_outdir_array[$lib_index]}/${project}.lib \
-${target_dir}/${install_dir_array[$lib_index]}/ 
+${target_dir}/${install_dir_array[$lib_index]}/
   done
 }
 
-# download/extract libogg and libvorbis distributions
-libogg_dir=$(wget_and_untar "${libogg_url}")
-#echo "libogg_dir=$libogg_dir"
-libvorbis_dir=$(wget_and_untar "${libvorbis_url}")
-#echo "libvorbis_dir=$libvorbis_dir"
 
-fix_project "${libogg_dir}win32/VS2008" libogg_static
-fix_project "${libvorbis_dir}win32/VS2008/libvorbis" libvorbis_static
 
-# build debug and release libs for win32 and x64 targets
-build_lib "${libogg_dir}" libogg_static libogg_static
-build_lib "${libvorbis_dir}" vorbis_static libvorbis_static
-# install libs and includes for webmdshow/webmmediafoundation
-install_files libogg_static "${libogg_dir}" libogg ogg
-install_files libvorbis_static "${libvorbis_dir}" libvorbis vorbis
 
-# clean up and remove a couple of unnecessary files
-rm -rf "${libogg_dir}" "${libvorbis_dir}" *.tar.gz libvorbis/vorbis/vorbis*.h
+# Xiph stuff
+if [[ -z "${disable_xiph_builds}" ]]; then
+  # download/extract libogg and libvorbis distributions
+  libogg_dir=$(wget_and_untar "${libogg_url}")
+  #echo "libogg_dir=$libogg_dir"
+  libvorbis_dir=$(wget_and_untar "${libvorbis_url}")
+  #echo "libvorbis_dir=$libvorbis_dir"
+  # Patch up the xiph projects w/a couple of fixes
+  # 1. use static run times
+  # 2. update ogg include path in vorbis proj
+  fix_xiph_project "${libogg_dir}win32/VS2008" libogg_static
+  fix_xiph_project "${libvorbis_dir}win32/VS2008/libvorbis" libvorbis_static
+  # build debug and release libs for win32 and x64 targets
+  build_xiph_lib "${libogg_dir}" libogg_static libogg_static
+  build_xiph_lib "${libvorbis_dir}" vorbis_static libvorbis_static
+  # install libs and includes for webmdshow/webmmediafoundation
+  install_xiph_files libogg_static "${libogg_dir}" libogg ogg
+  install_xiph_files libvorbis_static "${libvorbis_dir}" libvorbis vorbis
+  # clean up and remove a couple of unnecessary files
+  rm -rf "${libogg_dir}" "${libvorbis_dir}" *.tar.gz libvorbis/vorbis/vorbis*.h
+fi
 
 
