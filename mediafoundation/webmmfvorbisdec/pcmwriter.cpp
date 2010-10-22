1d76e1162c05ea6882353836ef22f4b3d685280e
Tom Finegan <tomfinegan@google.com>
2010-10-22 12:55:42 -0400

    webmmfvorbisdec: add simple PCM writer
    - enables when _DEBUG is defined
    - see pcmwriter.hpp for brief format description
    
    Change-Id: Icf661582b79c737c83a9c069474a91321d44fb32

---
+++ mediafoundation/webmmfvorbisdec/pcmwriter.cpp
@@ -0,0 +1,91 @@
+#ifdef _DEBUG
+#include <cassert>
+#include <vector>
+#ifdef _WIN32
+#include "mferror.h"
+#include "windows.h"
+#include "mmsystem.h"
+#include "mmreg.h"
+#endif
+#include "pcmwriter.hpp"
+
+namespace WebmMfVorbisDecLib
+{
+
+WfxPcmWriter::WfxPcmWriter() : closed_(true)
+{
+  ::memset(&wave_format_, 0, sizeof WAVEFORMATEX);
+}
+
+WfxPcmWriter::~WfxPcmWriter()
+{
+  Close();
+}
+
+int WfxPcmWriter::Open(const WAVEFORMATEX* ptr_wave_format)
+{
+  if (!ptr_wave_format)
+    return E_POINTER;
+
+  file_name_ = "out.wfxpcm";
+  using std::ios;
+  file_stream_.open(file_name_.c_str(), ios::binary);
+
+  int result = E_FAIL;
+  closed_ = !file_stream_.is_open();
+  if (!closed_)
+  {
+    file_stream_ << 'W' << 'F' << 'X';
+    const UINT32 num_bytes_to_write =
+      sizeof WAVEFORMATEX + ptr_wave_format->cbSize;
+    const char* const ptr_wfx_data = (char*)ptr_wave_format;
+    file_stream_.write(ptr_wfx_data, num_bytes_to_write);
+    result = Close();
+  }
+  return result;
+}
+
+int WfxPcmWriter::Write(const BYTE* const ptr_byte_buffer, UINT32 byte_count)
+{
+  assert(ptr_byte_buffer);
+  assert(byte_count > 0);
+  if (!ptr_byte_buffer)
+    return E_POINTER;
+  if (byte_count <= 0)
+    return E_INVALIDARG;
+  if (Reopen_() == S_OK)
+  {
+    const char* const ptr_char_buffer = (const char*)ptr_byte_buffer;
+    file_stream_.write(ptr_char_buffer, byte_count);
+    Close();
+  }
+  return S_OK;
+}
+
+int WfxPcmWriter::Reopen_()
+{
+  using std::ios;
+  file_stream_.open(file_name_.c_str(), ios::binary | ios::ate | ios::app);
+  closed_ = !file_stream_.is_open();
+  assert(!closed_);
+  if (closed_)
+    return E_FAIL;
+  return S_OK;
+}
+
+int WfxPcmWriter::Close()
+{
+  int result = S_FALSE;
+  if (!closed_)
+  {
+    file_stream_.flush();
+    file_stream_.close();
+    closed_ = true;
+    result = S_OK;
+  }
+  return result;
+}
+
+} // WebmMfVorbisDecLib
+
+#endif // _DEBUG
---
+++ mediafoundation/webmmfvorbisdec/pcmwriter.hpp
@@ -0,0 +1,43 @@
+#ifndef __MEDIAFOUNDATION_WEBMMFVORBISDEC_PCMWRITER_HPP__
+#define __MEDIAFOUNDATION_WEBMMFVORBISDEC_PCMWRITER_HPP__
+
+#ifndef DISALLOW_COPY_AND_ASSIGN
+#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
+  TypeName(const TypeName&);               \
+  void operator=(const TypeName&)
+#endif
+
+
+#ifdef _DEBUG
+#include <fstream>
+#include <ios>
+#include <string>
+
+namespace WebmMfVorbisDecLib
+{
+
+// Writes a file in the following format:
+// ------------------------------------------------------
+// | WAVEFORMATEX | WAVEFORMATEX.cbSize bytes | samples |
+// ------------------------------------------------------
+class WfxPcmWriter
+{
+public:
+    WfxPcmWriter();
+    ~WfxPcmWriter();
+    int Open(const WAVEFORMATEX* ptr_wave_format);
+    int Write(const BYTE* const ptr_byte_buffer, UINT32 byte_count);
+    int Close();
+private:
+    int Reopen_();
+    bool closed_;
+    WAVEFORMATEX wave_format_;
+    std::ofstream file_stream_;
+    std::string file_name_;
+    DISALLOW_COPY_AND_ASSIGN(WfxPcmWriter);
+};
+
+}
+#endif // _DEBUG
+
+#endif // __MEDIAFOUNDATION_WEBMMFVORBISDEC_PCMWRITER_HPP__
