81bef10333bb7cee989af323402d57ac81365604
Tom Finegan <tomfinegan@google.com>
2011-03-01 01:01:46 -0500

    webmmfsource: implement usage tracking
    - Tracks usage between updates via Omaha registry keys.
    - Tracking enabled only if elected by user at install time
    
    Change-Id: Id79de5f946636fea23485f7e8454bd68dee30d3c

---
+++ common/omahautil.hpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2011 The WebM project authors. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+
+#ifndef __WEBMDSHOW_COMMON_OMAHAUTIL_HPP__
+#define __WEBMDSHOW_COMMON_OMAHAUTIL_HPP__
+
+#include <comdef.h>
+
+namespace WebmUtil
+{
+
+// Super simple public interface... just give us the App ID GUID
+HRESULT set_omaha_usage_flags(const GUID& app_id);
+
+} // WebmUtil namespace
+
+#endif // __WEBMDSHOW_COMMON_OMAHAUTIL_HPP__
