74379b419a791c5d81f1120c0f23e28d19cf03eb
Matthew Heaney <matthewjheaney@chromium.org>
2013-10-03 15:14:00 -0700

    webmmf: version 1.0.1.0
    Change-Id: Id2178bb31edbc2261523d9ccd777537fd6bfed6b

--- mediafoundation/webmmfvorbisdec/resource.h
+++ mediafoundation/webmmfvorbisdec/resource.h
@@ -1,9 +1,10 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by webmmfvorbisdec_2008.rc
+// Used by webmmfvorbisdec.rc
+//
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        101
