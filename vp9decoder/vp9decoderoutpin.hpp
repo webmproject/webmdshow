04a4b94b5e516844d9592cde68d6a31e8bc04133
Tom Finegan <tomfinegan@google.com>
2014-11-14 17:02:43 -0800

    webmdshow clean up: clang-format vp9decoder/vp9decoderinpin.{c,h}pp
    Change-Id: I8792cba779ad0c1c16a680cf0181be7974aca387

--- vp9decoder/vp9decoderoutpin.hpp
+++ vp9decoder/vp9decoderoutpin.hpp
@@ -5,155 +5,103 @@
 // tree. An additional intellectual property rights grant can be found
 // in the file PATENTS.  All contributing project authors may
 // be found in the AUTHORS file in the root of the source tree.
+#ifndef WEBDSHOW_VP9DECODER_VP9DECODEROUTPIN_HPP_
+#define WEBDSHOW_VP9DECODER_VP9DECODEROUTPIN_HPP_
 
-#pragma once
-#include "vp9decoderpin.hpp"
+#include <amvideo.h>
 #include <comdef.h>
-#include "graphutil.hpp"
+#include <strmif.h>
 
-namespace VP9DecoderLib
-{
+#include "graphutil.hpp"
+#include "vp9decoderpin.hpp"
+
+namespace VP9DecoderLib {
 
 class Filter;
 
-class Outpin : public Pin,
-               public IMediaSeeking
-{
-    Outpin(const Outpin&);
-    Outpin& operator=(const Outpin&);
+class Outpin : public Pin, public IMediaSeeking {
+ public:
+  explicit Outpin(Filter*);
+  virtual ~Outpin();
 
-public:
-    explicit Outpin(Filter*);
-    virtual ~Outpin();
+  // IUnknown interface:
+  HRESULT STDMETHODCALLTYPE QueryInterface(const IID&, void**);
+  ULONG STDMETHODCALLTYPE AddRef();
+  ULONG STDMETHODCALLTYPE Release();
 
-    HRESULT Start();  //from stopped to running/paused
-    void Stop();      //from running/paused to stopped
+  // IPin interface:
+  HRESULT STDMETHODCALLTYPE Connect(IPin*, const AM_MEDIA_TYPE*);
+  HRESULT STDMETHODCALLTYPE ReceiveConnection(IPin*, const AM_MEDIA_TYPE*);
+  HRESULT STDMETHODCALLTYPE QueryAccept(const AM_MEDIA_TYPE*);
+  HRESULT STDMETHODCALLTYPE QueryInternalConnections(IPin**, ULONG*);
+  HRESULT STDMETHODCALLTYPE EndOfStream();
+  HRESULT STDMETHODCALLTYPE BeginFlush();
+  HRESULT STDMETHODCALLTYPE EndFlush();
+  HRESULT STDMETHODCALLTYPE NewSegment(REFERENCE_TIME, REFERENCE_TIME, double);
 
-    //IUnknown interface:
+  // IMediaSeeking
+  HRESULT STDMETHODCALLTYPE GetCapabilities(DWORD*);
+  HRESULT STDMETHODCALLTYPE CheckCapabilities(DWORD*);
+  HRESULT STDMETHODCALLTYPE IsFormatSupported(const GUID*);
+  HRESULT STDMETHODCALLTYPE QueryPreferredFormat(GUID*);
+  HRESULT STDMETHODCALLTYPE GetTimeFormat(GUID*);
+  HRESULT STDMETHODCALLTYPE IsUsingTimeFormat(const GUID*);
+  HRESULT STDMETHODCALLTYPE SetTimeFormat(const GUID*);
+  HRESULT STDMETHODCALLTYPE GetDuration(LONGLONG*);
+  HRESULT STDMETHODCALLTYPE GetStopPosition(LONGLONG*);
+  HRESULT STDMETHODCALLTYPE GetCurrentPosition(LONGLONG*);
+  HRESULT STDMETHODCALLTYPE
+      ConvertTimeFormat(LONGLONG*, const GUID*, LONGLONG, const GUID*);
+  HRESULT STDMETHODCALLTYPE SetPositions(LONGLONG*, DWORD, LONGLONG*, DWORD);
+  HRESULT STDMETHODCALLTYPE GetPositions(LONGLONG*, LONGLONG*);
+  HRESULT STDMETHODCALLTYPE GetAvailable(LONGLONG*, LONGLONG*);
+  HRESULT STDMETHODCALLTYPE SetRate(double);
+  HRESULT STDMETHODCALLTYPE GetRate(double*);
+  HRESULT STDMETHODCALLTYPE GetPreroll(LONGLONG*);
 
-    HRESULT STDMETHODCALLTYPE QueryInterface(const IID&, void**);
-    ULONG STDMETHODCALLTYPE AddRef();
-    ULONG STDMETHODCALLTYPE Release();
+  // local functions
+  HRESULT Start();  // from stopped to running/paused
+  void Stop();  // from running/paused to stopped
+  void OnInpinConnect(const AM_MEDIA_TYPE&);
+  HRESULT OnInpinDisconnect();
 
-    //IPin interface:
+  // public members
+  GraphUtil::IMemInputPinPtr m_pInputPin;
+  GraphUtil::IMemAllocatorPtr m_pAllocator;
 
-    //HRESULT STDMETHODCALLTYPE EnumMediaTypes(IEnumMediaTypes**);
+ protected:
+  HRESULT GetName(PIN_INFO&) const;
+  HRESULT OnDisconnect();
 
-    HRESULT STDMETHODCALLTYPE Connect(IPin*, const AM_MEDIA_TYPE*);
+ private:
+  void SetDefaultMediaTypes();
 
-    //HRESULT STDMETHODCALLTYPE Disconnect();
+  static HRESULT QueryAcceptVideoInfo(const AM_MEDIA_TYPE& mt_in,
+                                      const AM_MEDIA_TYPE& mt_out);
 
-    HRESULT STDMETHODCALLTYPE ReceiveConnection(
-        IPin*,
-        const AM_MEDIA_TYPE*);
+  static HRESULT QueryAcceptVideoInfo2(const AM_MEDIA_TYPE& mt_in,
+                                       const AM_MEDIA_TYPE& mt_out);
 
-    HRESULT STDMETHODCALLTYPE QueryAccept(const AM_MEDIA_TYPE*);
+  static bool VetBMIH(const VIDEOINFOHEADER& vih_in, const GUID& subtype_out,
+                      const RECT& rc_out, const BITMAPINFOHEADER& bmih_out);
 
-    HRESULT STDMETHODCALLTYPE QueryInternalConnections(
-        IPin**,
-        ULONG*);
+  void GetConnectionDimensions(LONG& w, LONG& h) const;
 
-    HRESULT STDMETHODCALLTYPE EndOfStream();
+  void AddPreferred(const GUID& subtype, REFERENCE_TIME AvgTimePerFrame,
+                    LONG width, LONG height, DWORD dwBitCount,
+                    DWORD dwSizeImage);
 
-    HRESULT STDMETHODCALLTYPE BeginFlush();
-    HRESULT STDMETHODCALLTYPE EndFlush();
+  static void AddVIH(CMediaTypes&, const GUID& subtype,
+                     REFERENCE_TIME AvgTimePerFrame, LONG width, LONG height,
+                     DWORD dwBitCount, DWORD dwSizeImage);
 
-    HRESULT STDMETHODCALLTYPE NewSegment(
-        REFERENCE_TIME,
-        REFERENCE_TIME,
-        double);
-
-    //IMediaSeeking
-
-    HRESULT STDMETHODCALLTYPE GetCapabilities(DWORD*);
-    HRESULT STDMETHODCALLTYPE CheckCapabilities(DWORD*);
-    HRESULT STDMETHODCALLTYPE IsFormatSupported(const GUID*);
-    HRESULT STDMETHODCALLTYPE QueryPreferredFormat(GUID*);
-    HRESULT STDMETHODCALLTYPE GetTimeFormat(GUID*);
-    HRESULT STDMETHODCALLTYPE IsUsingTimeFormat(const GUID*);
-    HRESULT STDMETHODCALLTYPE SetTimeFormat(const GUID*);
-    HRESULT STDMETHODCALLTYPE GetDuration(LONGLONG*);
-    HRESULT STDMETHODCALLTYPE GetStopPosition(LONGLONG*);
-    HRESULT STDMETHODCALLTYPE GetCurrentPosition(LONGLONG*);
-
-    HRESULT STDMETHODCALLTYPE ConvertTimeFormat(
-        LONGLONG*,
-        const GUID*,
-        LONGLONG,
-        const GUID*);
-
-    HRESULT STDMETHODCALLTYPE SetPositions(
-        LONGLONG*,
-        DWORD,
-        LONGLONG*,
-        DWORD);
-
-    HRESULT STDMETHODCALLTYPE GetPositions(
-        LONGLONG*,
-        LONGLONG*);
-
-    HRESULT STDMETHODCALLTYPE GetAvailable(
-        LONGLONG*,
-        LONGLONG*);
-
-    HRESULT STDMETHODCALLTYPE SetRate(double);
-    HRESULT STDMETHODCALLTYPE GetRate(double*);
-    HRESULT STDMETHODCALLTYPE GetPreroll(LONGLONG*);
-
-    //local functions
-
-    GraphUtil::IMemInputPinPtr m_pInputPin;
-    GraphUtil::IMemAllocatorPtr m_pAllocator;
-
-    void OnInpinConnect(const AM_MEDIA_TYPE&);
-    HRESULT OnInpinDisconnect();
-
-protected:
-    HRESULT GetName(PIN_INFO&) const;
-    HRESULT OnDisconnect();
-
-private:
-    void SetDefaultMediaTypes();
-
-    static HRESULT QueryAcceptVideoInfo(
-      const AM_MEDIA_TYPE& mt_in,
-      const AM_MEDIA_TYPE& mt_out);
-
-    static HRESULT QueryAcceptVideoInfo2(
-      const AM_MEDIA_TYPE& mt_in,
-      const AM_MEDIA_TYPE& mt_out);
-
-    static bool VetBMIH(
-      const VIDEOINFOHEADER& vih_in,
-      const GUID& subtype_out,
-      const RECT& rc_out,
-      const BITMAPINFOHEADER& bmih_out);
-
-    void GetConnectionDimensions(LONG& w, LONG& h) const;
-
-    void AddPreferred(const GUID& subtype,
-                      REFERENCE_TIME AvgTimePerFrame,
-                      LONG width,
-                      LONG height,
-                      DWORD dwBitCount,
-                      DWORD dwSizeImage);
-
-    static void AddVIH(CMediaTypes&,
-                       const GUID& subtype,
-                       REFERENCE_TIME AvgTimePerFrame,
-                       LONG width,
-                       LONG height,
-                       DWORD dwBitCount,
-                       DWORD dwSizeImage);
-
-    static void AddVIH2(CMediaTypes&,
-                        const GUID& subtype,
-                        REFERENCE_TIME AvgTimePerFrame,
-                        LONG width,
-                        LONG height,
-                        DWORD dwBitCount,
-                        DWORD dwSizeImage);
-
+  static void AddVIH2(CMediaTypes&, const GUID& subtype,
+                      REFERENCE_TIME AvgTimePerFrame, LONG width, LONG height,
+                      DWORD dwBitCount, DWORD dwSizeImage);
+  Outpin(const Outpin&);
+  Outpin& operator=(const Outpin&);
 };
 
-}  //end namespace VP9DecoderLib
+}  // end namespace VP9DecoderLib
+
+#endif  // WEBDSHOW_VP9DECODER_VP9DECODEROUTPIN_HPP_
