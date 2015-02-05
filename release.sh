ea5478aa98c29a53934b8fa710a6d0f26c80fecb
Tom Finegan <tomfinegan@google.com>
2015-02-05 11:17:36 -0800

    Use common.sh in release.sh.
    Change-Id: Idacb7852e23443a396d1cad32cfcfe563c3cd22f

--- release.sh
+++ release.sh
@@ -8,20 +8,7 @@
 ##  in the file PATENTS.  All contributing project authors may
 ##  be found in the AUTHORS file in the root of the source tree.
 ##
-set -e
-devnull='> /dev/null 2>&1'
-
-readonly ORIG_PWD="$(pwd)"
-
-elog() {
-  echo "${0##*/} failed because: $@" 1>&2
-}
-
-vlog() {
-  if [ "${VERBOSE}" = "yes" ]; then
-    echo "$@"
-  fi
-}
+. $(dirname $0)/../common/common.sh
 
 cleanup() {
   local readonly res=$?
